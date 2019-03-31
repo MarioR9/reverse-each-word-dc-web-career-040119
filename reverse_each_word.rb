6e97ec9929c6fc7e09caba9c5592b872a69cea88
mario rodrigueza <mariorodriguezan@gmail.com>
2019-03-31 23:55:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(string)
   # spli the string
     easy_array = string.split(" ")
     new_array = []
-    easy_array.collect do |link|
-      new_array.push link.reverse
+    easy_array.collect do |word|
+      new_array.push word.reverse
   end
   new_array.join(" ")
 
